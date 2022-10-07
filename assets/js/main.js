1a1064ce7768b006c421932bdac2ee13b53e81f7
Pasquale Aragosa <aragosap@gmail.com>
2022-10-07 16:13:17 +0200

    add click me

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,28 @@
-const userKm = prompt("Digita il numero di chilometri da percorrere: ");
-console.log(userKm);
-const userAge = prompt("Digita il numero di chilometri da percorrere: ");
-console.log(userAge);
-const priceForKm = 0.21;
-let check;
-
-if (userAge > 65) {
-    check = (userKm * priceForKm) * 0.6;
-} else if (userAge < 18) {
-    check = (userKm * priceForKm) * 0.8;
-} else {
-    check = userKm * priceForKm;
-}
+const buttonTicket = document.getElementById('ticket');
 
 
+buttonTicket.addEventListener('click', 
+    function () {
+        console.log('Mi ha cliccato');
+    }
+)
+
+
+
+// const userKm = prompt("Digita il numero di chilometri da percorrere: ");
+// console.log(userKm);
+// const userAge = prompt("Digita il numero di chilometri da percorrere: ");
+// console.log(userAge);
+// const priceForKm = 0.21;
+// let check;
+
+// if (userAge > 65) {
+//     check = (userKm * priceForKm) * 0.6;
+// } else if (userAge < 18) {
+//     check = (userKm * priceForKm) * 0.8;
+// } else {
+//     check = userKm * priceForKm;
+// }
+
+// console.log(check);
 
