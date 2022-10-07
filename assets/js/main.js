fb25e8f6658cea7e476f40a0ab61e0ba5cc21823
Pasquale Aragosa <aragosap@gmail.com>
2022-10-07 15:29:26 +0200

    inizial commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const userKm = prompt("Digita il numero di chilometri da percorrere: ");
+console.log(userKm);
+const userAge = prompt("Digita il numero di chilometri da percorrere: ");
+console.log(userAge);
+const priceForKm = 0.21;
+let check;
+
+if (userAge > 65) {
+    check = (userKm * priceForKm) * 0.6;
+} else if (userAge < 18) {
+    check = (userKm * priceForKm) * 0.8;
+} else {
+    check = userKm * priceForKm;
+}
+
+
+
