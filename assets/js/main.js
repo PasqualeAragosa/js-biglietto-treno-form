d2ae23fc6547c3f7fb53806e16d31a7a8fd463ba
Pasquale Aragosa <aragosap@gmail.com>
2022-10-07 16:27:16 +0200

    add writln

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,25 @@
+let userKm;
+let userAge;
+let check;
+const priceForKm = 0.21;
 const buttonTicket = document.getElementById('ticket');
-
 
 buttonTicket.addEventListener('click', 
     function () {
-        console.log('Mi ha cliccato');
+        userKm = document.getElementById('user_km').value;
+        userAge = document.getElementById('user_age').value;
+
+        if (userAge > 65) {
+            check = (userKm * priceForKm) * 0.6;
+        } else if (userAge < 18) {
+            check = (userKm * priceForKm) * 0.8;
+        } else {
+            check = userKm * priceForKm;
+        }
+        
+        document.writeln("Prezzo Biglietto: €", check.toFixed(2));
     }
 )
 
 
-
-// const userKm = prompt("Digita il numero di chilometri da percorrere: ");
-// console.log(userKm);
-// const userAge = prompt("Digita il numero di chilometri da percorrere: ");
-// console.log(userAge);
-// const priceForKm = 0.21;
-// let check;
-
-// if (userAge > 65) {
-//     check = (userKm * priceForKm) * 0.6;
-// } else if (userAge < 18) {
-//     check = (userKm * priceForKm) * 0.8;
-// } else {
-//     check = userKm * priceForKm;
-// }
-
-// console.log(check);
 
